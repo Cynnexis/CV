24a47f8a8e817c1a1f3143fdaca333734bd8ea3b
Valentin Berger <Valentin.berger38@gmail.com>
2019-09-09 14:44:34 +0200

    :sparkles: Finish Change Color for primary color
    * The accent color can now be changed in `resume.cls` as well.

--- change-color.py
+++ change-color.py
@@ -11,7 +11,7 @@ IMAGES_FOLDER_NAME = "images"
 
 # Parse the arguments
 p = argparse.ArgumentParser(prog="Change Color", description="Change the colors of the images and CVs.")
-p.add_argument("-a", "--accent-color", default=None, help="The accent color. Default is cyan (#45818E).")
+p.add_argument("-p", "--primary-color", default=None, help="The accent color. Default is cyan (#45818E).")
 args = p.parse_args()
 
 # Get the images
@@ -26,26 +26,32 @@ if IMAGES_FOLDER_NAME in current_folder_entries and os.path.isdir(IMAGES_FOLDER_
 			images_path.append(os.path.abspath(os.path.join(IMAGES_FOLDER_NAME, images_entry)))
 
 # Convert accent color
-if args.accent_color is not None:
-	if args.accent_color.startswith("0x"):
-		args.accent_color = args.accent_color.replace("0x", '#')
-	elif not args.accent_color.startswith('#'):
-		args.accent_color = '#' + args.accent_color
+if args.primary_color is not None:
+	if args.primary_color.startswith("0x"):
+		args.primary_color = args.primary_color.replace("0x", '#')
+	elif not args.primary_color.startswith('#'):
+		args.primary_color = '#' + args.primary_color
 	
 	# Get the old accent color
 	old_accent_color = "45818E"
-	with open("resume.cls", 'r') as f:
+	with open("resume.cls", "r+") as f:
 		resume_cls = f.read()
-	color_matches = re.findall(r"\\definecolor{primary-color}{HTML}{#?([0-9a-fA-F]{6})}", resume_cls, re.MULTILINE)
-	if len(color_matches) > 0:
-		old_accent_color = color_matches[0]
+		color_matches = re.findall(r"\\definecolor{primary-color}{HTML}{#?([0-9a-fA-F]{6})}", resume_cls, re.MULTILINE)
+		if len(color_matches) > 0:
+			old_accent_color = color_matches[0]
+		# Replace occurrence in resume.cls
+		new_resume_cls, nb_chgmt = re.subn(r"\\definecolor{primary-color}{HTML}{" + old_accent_color + r'}', r"\\definecolor{primary-color}{HTML}{" + re.sub("(#|0x)", '', args.primary_color) + "}", resume_cls, re.IGNORECASE)
+		if nb_chgmt > 0:
+			f.seek(0)
+			f.write(new_resume_cls)
+			f.truncate()
 	
 	# Replace occurrences in images (svg)
 	print("Coloring images...")
 	for image_path in images_path:
 		with open(image_path, 'r+') as f:
 			image_content = f.read()
-			new_image_content, nb_chgmt = re.subn('#' + old_accent_color, args.accent_color, image_content, flags=re.IGNORECASE)
+			new_image_content, nb_chgmt = re.subn('#' + old_accent_color, args.primary_color, image_content, flags=re.IGNORECASE)
 			if nb_chgmt > 0:
 				f.seek(0)
 				f.write(new_image_content)
